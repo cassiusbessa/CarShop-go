2889fc8fdabb1ba8ef8571cdd7d5255688cc4c13
Cassius <cassiusbessa@gmail.com>
2022-10-16 20:45:32 -0300

    feat(db): :art: implements car entity

---
+++ internal/db/entities/cars.go
@@ -0,0 +1,26 @@
+package entity
+
+import (
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type ICar struct {
+	Id       primitive.ObjectID `json:"Id,omitempty" bson:"_id,omitempty"`
+	Model    string             `json:"model,omitempty" bson:"model,omitempty" validate:"required"`
+	Year     int                `json:"year,omitempty" bson:"year,omitempty" validate:"required"`
+	Color    string             `json:"color,omitempty" bson:"color,omitempty" validate:"required"`
+	Status   bool               `json:"status,omitempty" bson:"status,omitempty" validate:"required"`
+	BuyValue float64            `json:"buyValue,omitempty" bson:"buyValue,omitempty" validate:"required"`
+	DoorsQty int                `json:"doorsQty,omitempty" bson:"doorsQty,omitempty" validate:"required,gte=1,lte=5"`
+	SeatsQty int                `json:"seatsQty,omitempty" bson:"seatsQty,omitempty" validate:"required,gte=1,lte=10"`
+}
+
+func (c *ICar) Validate() error {
+	valid := validator.New()
+	err := valid.Struct(c)
+	if err != nil {
+		return err
+	}
+	return nil
+}
