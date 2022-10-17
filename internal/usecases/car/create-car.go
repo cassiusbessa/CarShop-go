be6b7823b9b4464e50d013e0b3d0af0ea70d1cdd
Cassius <cassiusbessa@gmail.com>
2022-10-16 21:21:32 -0300

    feat(useCase): :sparkles: implements createCar  use case's method

---
+++ internal/usecases/car/create-car.go
@@ -0,0 +1,17 @@
+package car_use_case
+
+import (
+	"context"
+	"net/http"
+
+	entity "github.com/cassiusbessa/goCarShop/internal/db/entities"
+	"github.com/cassiusbessa/goCarShop/internal/utils"
+)
+
+func (c *carUseCase) CreateCar(ctx context.Context, car entity.ICar) (*entity.ICar, *utils.CustomError) {
+	err := car.Validate()
+	if err != nil {
+		return &entity.ICar{}, utils.NewError(http.StatusBadRequest, err.Error(), "use-case-create-car", err)
+	}
+	return c.carRepo.CreateCar(ctx, car)
+}
