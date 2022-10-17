5737a0cd081fafabd2221d8fd1a4adf271417bc2
Cassius <cassiusbessa@gmail.com>
2022-10-16 21:48:33 -0300

    feat(controller): :sparkles: implements createCar controller's method

---
+++ internal/controllers/car/create-car.go
@@ -0,0 +1,29 @@
+package car_controller
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	entity "github.com/cassiusbessa/goCarShop/internal/db/entities"
+	"github.com/cassiusbessa/goCarShop/internal/utils"
+)
+
+func (c *carController) CreateCar(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("Content-Type", "application/json")
+	var car *entity.ICar
+	err := json.NewDecoder(req.Body).Decode(&car)
+	if err != nil {
+		err := utils.NewError(http.StatusInternalServerError, "controller-car-create", "Error decoding car", err)
+		utils.SendError(err, res)
+		return
+	}
+	car, error := c.carUseCase.CreateCar(context.Background(), *car)
+	if err != nil {
+		utils.SendError(error, res)
+		return
+	}
+	res.WriteHeader(http.StatusCreated)
+	defer json.NewEncoder(res).Encode(car)
+
+}
