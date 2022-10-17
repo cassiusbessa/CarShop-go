3f99f7a5aedffc8abd4049b90e6af3be77586325
Cassius <cassiusbessa@gmail.com>
2022-10-16 21:09:59 -0300

    feat(repositories): :sparkles: implements createCar repository's method

---
+++ internal/db/repositories/car/create-car.go
@@ -0,0 +1,24 @@
+package car_repo
+
+import (
+	"context"
+	"net/http"
+
+	entity "github.com/cassiusbessa/goCarShop/internal/db/entities"
+	"github.com/cassiusbessa/goCarShop/internal/utils"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func (c *carRepo) CreateCar(ctx context.Context, car entity.ICar) (*entity.ICar, *utils.CustomError) {
+	car.Id = primitive.NewObjectID()
+	err := car.Validate()
+	if err != nil {
+		return &entity.ICar{}, utils.NewError(http.StatusBadRequest, err.Error(), "repo-create-car", err)
+	}
+	_, err = c.collection.InsertOne(ctx, car)
+	if err != nil {
+		return &entity.ICar{},
+			utils.NewError(http.StatusInternalServerError, "db insert error", "repo-create-car", err)
+	}
+	return &car, nil
+}
