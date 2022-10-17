d0478a1ad5da83ad372c56ddcfc96eec6c3f85f8
Cassius <cassiusbessa@gmail.com>
2022-10-16 21:17:06 -0300

    feat(useCase): :art: implements car use case interface and struct

---
+++ internal/usecases/car/car-use-case.go
@@ -0,0 +1,24 @@
+package car_use_case
+
+import (
+	"context"
+
+	entity "github.com/cassiusbessa/goCarShop/internal/db/entities"
+	car_repo "github.com/cassiusbessa/goCarShop/internal/db/repositories/car"
+	"github.com/cassiusbessa/goCarShop/internal/utils"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type ICarUseCase interface {
+	CreateCar(ctx context.Context, car entity.ICar) (*entity.ICar, *utils.CustomError)
+	GetCar(ctx context.Context, id string) (*entity.ICar, *utils.CustomError)
+	GetAllCars(ctx context.Context) (*[]entity.ICar, *utils.CustomError)
+}
+
+type carUseCase struct {
+	carRepo car_repo.ICarRepository
+}
+
+func New(collection *mongo.Collection) ICarUseCase {
+	return &carUseCase{carRepo: car_repo.New(collection)}
+}
