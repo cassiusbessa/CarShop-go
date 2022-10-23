ece9b5d4e4f92233a9fff34d1ae0daefcce9d3db
Cassius <cassiusbessa@gmail.com>
2022-10-23 20:26:49 -0300

    feat(repositories): :sparkles: now update car returns the car with the updates

--- internal/db/repositories/car/update-car.go
+++ internal/db/repositories/car/update-car.go
@@ -8,10 +8,11 @@ import (
 	"github.com/cassiusbessa/goCarShop/internal/utils"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func (c *carRepo) UpdateCar(ctx context.Context, id primitive.ObjectID, car entity.ICar) (*entity.ICar, *utils.CustomError) {
-	r := c.collection.FindOneAndUpdate(context.Background(), bson.M{"_id": id}, bson.M{"$set": car})
+	r := c.collection.FindOneAndUpdate(context.Background(), bson.M{"_id": id}, bson.M{"$set": car}, options.FindOneAndUpdate().SetReturnDocument(1))
 	if r.Err() != nil {
 		return &entity.ICar{},
 			utils.NewError(http.StatusInternalServerError, "db update error", "repo-update-car", r.Err())
