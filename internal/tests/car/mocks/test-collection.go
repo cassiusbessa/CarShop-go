a1a808d74cf1a149e6e55a87d6ce918c86f85489
Cassius <cassiusbessa@gmail.com>
2022-10-23 20:35:40 -0300

    test(mocks): :sparkles: implements test Cars collection

---
+++ internal/tests/car/mocks/test-collection.go
@@ -0,0 +1,23 @@
+package mocks
+
+import (
+	context "context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func newCollection(db, collection string) *mongo.Collection {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+	}
+	// defer func() {
+	// 	if err := client.Disconnect(context.TODO()); err != nil {
+	// 		panic(err)
+	// 	}
+	// }()
+	return client.Database(db).Collection(collection)
+}
+
+var TestCollection = newCollection("test", "cars")
