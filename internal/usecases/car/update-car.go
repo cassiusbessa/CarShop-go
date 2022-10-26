adcdcad5e8febc9565b29fff3ebf3d5e835070b2
Cassius <cassiusbessa@gmail.com>
2022-10-26 04:03:13 -0300

    refactor(useCase): :zap: refactories validadetion of update car

--- internal/usecases/car/update-car.go
+++ internal/usecases/car/update-car.go
@@ -2,6 +2,7 @@ package car_use_case
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 	"reflect"
 
@@ -12,25 +13,21 @@ import (
 
 func (c *carUseCase) UpdateCar(ctx context.Context, id string, car entity.ICar) (*entity.ICar, *utils.CustomError) {
 	mongoId, _ := primitive.ObjectIDFromHex(id)
-	err := validateUpdate(car)
+	err := validateUpdateCar(car)
 	if err != nil {
 		return &entity.ICar{}, utils.NewError(http.StatusBadRequest, err.Error(), "use-case-update-car", err)
 	}
 	return c.carRepo.UpdateCar(ctx, mongoId, car)
 }
 
-func validateUpdate(car entity.ICar) *utils.CustomError {
-	v := reflect.ValueOf(car)
-	keys := reflect.VisibleFields(v.Type())
-	var validateKeys []string
-	for _, key := range keys {
-		if !(v.FieldByName(key.Name).IsZero()) {
-			validateKeys = append(validateKeys, key.Name)
-		}
+func validateUpdateCar(car entity.ICar) error {
+	var inInterface map[string]interface{}
+	carrec, _ := json.Marshal(car)
+	json.Unmarshal(carrec, &inInterface)
+	k := reflect.ValueOf(inInterface).MapKeys()
+	keys := make([]string, len(k))
+	for i, a := range k {
+		keys[i] = a.String()
 	}
-	err := car.UpdateValidate(validateKeys...)
-	if err != nil {
-		return utils.NewError(http.StatusBadRequest, err.Error(), "use-case-update-car", err)
-	}
-	return nil
+	return car.UpdateValidate(keys...)
 }
